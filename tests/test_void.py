7257fa5e516bf1ec15c41f82096325ef1f60acd1
Shota Imaki <shota.imaki@icloud.com>
2020-01-20 21:30:37 +0900

    add test

--- tests/test_void.py
+++ tests/test_void.py
@@ -1,5 +1,30 @@
+"""
+Test for a case where no trade has been yielded.
+"""
 import pytest
+
+import pandas as pd
+import numpy as np
+
+from epymetheus import TradeStrategy, Universe
+from epymetheus.datasets import make_randomwalk
+
+
+class VoidStrategy(TradeStrategy):
+    """Yield no trade."""
+    def logic(self, universe):
+        pass
 
 
 def test_void():
-    assert 1 == 1
+    strategy = VoidStrategy()
+    universe = make_randomwalk()
+    strategy.run(universe)
+
+    frame_history = pd.DataFrame(strategy.history)
+    frame_transaction = pd.DataFrame(strategy.transaction)
+    frame_wealth = pd.DataFrame(strategy.wealth)
+
+    assert len(frame_history.index) == 0
+    assert (frame_transaction == 0).all(None)
+    assert (frame_wealth == 0).all(None)
