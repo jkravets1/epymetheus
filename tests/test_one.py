7192dbe316e58b200822f8e3892b9076d69e1b5a
Shota Imaki <shota.imaki@icloud.com>
2020-01-27 21:53:20 +0900

    refactored test

--- tests/test_one.py
+++ tests/test_one.py
@@ -1,8 +1,6 @@
-"""
-Basic test with a single trade.
-"""
 import pytest
 
+import random
 import pandas as pd
 import numpy as np
 
@@ -10,26 +8,50 @@ from epymetheus import Trade, TradeStrategy, Universe
 from epymetheus.datasets import make_randomwalk
 
 
+list_seed = [42, 1, 2, 3]
+list_n_bars = [10, 1000]
+list_n_assets = [1, 100]
+list_lot = [0.0, 1, 1.23, -1.0, 123.4, -123.4]
+
+
 class OneTradeStrategy(TradeStrategy):
-    """Yield one trade."""
+    """
+    Yield one trade.
+    """
     def logic(self, universe, asset, lot, open_bar, close_bar):
         yield Trade(
             asset=asset, lot=lot, open_bar=open_bar, close_bar=close_bar,
         )
 
 
-def test_one():
-    universe = make_randomwalk()
+def make_randomuniverse(n_bars, n_assets):
+    assets = np.arange(n_assets).astype(str)
+    prices = pd.DataFrame(
+        100 + np.random.randn(n_bars, n_assets).cumsum(axis=0),
+        columns=assets,
+    )
+    return Universe(prices)
 
-    asset = universe.assets[0]
-    lot = 12.3
-    open_bar = universe.bars[0]
-    close_bar = universe.bars[-1]
+
+# --------------------------------------------------------------------------------
+
+
+@pytest.mark.parametrize('seed', list_seed)
+@pytest.mark.parametrize('n_bars', list_n_bars)
+@pytest.mark.parametrize('n_assets', list_n_assets)
+@pytest.mark.parametrize('lot', list_lot)
+def test_one(seed, n_bars, n_assets, lot):
+    np.random.seed(seed)
+    random.seed(seed)
+
+    universe = make_randomuniverse(n_bars, n_assets)
+
+    asset = random.choice(universe.assets)
+    open_bar, close_bar = sorted(random.sample(list(universe.bars), 2))
 
     strategy = OneTradeStrategy(
         asset=asset, lot=lot, open_bar=open_bar, close_bar=close_bar,
     )
-
     strategy.run(universe)
 
     open_price = universe.prices.at[open_bar, asset]
@@ -46,7 +68,3 @@ def test_one():
     assert strategy.history.open_prices == np.array([open_price])
     assert strategy.history.close_prices == np.array([close_price])
     assert strategy.history.gains == np.array([gain])
-
-    # assert strategy.transaction[asset] ==
-
-    # assert strategy.wealth.wealth ==
