542387eb12d9585806d596b30b7e8e2999790488
Shota Imaki <shota.imaki@icloud.com>
2020-01-28 08:32:36 +0900

    add test

--- tests/test_trade.py
+++ tests/test_trade.py
@@ -32,3 +32,14 @@ def test_rmul(lot, a):
     assert trade_mul.open_bar == trade.open_bar
     assert trade_mul.close_bar == trade.close_bar
     assert trade_mul.lot == a * trade.lot
+
+
+@pytest.mark.parametrize('lot', list_lot)
+def test_neg(lot):
+    trade = Trade(asset='asset0', open_bar='bar0', close_bar='bar1', lot=lot)
+    trade_mul = - trade
+
+    assert trade_mul.asset == trade.asset
+    assert trade_mul.open_bar == trade.open_bar
+    assert trade_mul.close_bar == trade.close_bar
+    assert trade_mul.lot == (-1) * trade.lot
