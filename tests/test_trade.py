70f8952d7fda7763ab68572fdf08ba40b0c4eb6f
Shota Imaki <shota.imaki@icloud.com>
2020-01-28 09:34:28 +0900

    add trade operator tests

--- tests/test_trade.py
+++ tests/test_trade.py
@@ -1,45 +1,84 @@
 import pytest
 
+from random import choice, choices
 from epymetheus import Trade
 
 
-list_lot = [0.0, 1, 1.23, -1.23, 123.4, -123.4]
+lots = [0.0, 1, 1.23, -1.23, 123.4, -123.4]
+
+list_n_orders = [1, 5]
 list_a = [0.0, 1, 1.23, -1.23, 123.4, -123.4]
+
+
+def make_trade(n_orders):
+    if n_orders == 1:
+        asset = 'asset0'
+        lot = choice(lots)
+    else:
+        asset=[f'asset{i}' for i in range(n_orders)],
+        lot=choices(lots, k=n_orders)
+    return Trade(
+        asset=asset, open_bar='bar0', close_bar='bar1', lot=lot,
+    )
+
+
+def assert_trade_operation(trade0, trade1, operator):
+    assert trade1.asset == trade0.asset
+    assert trade1.open_bar == trade0.open_bar
+    assert trade1.close_bar == trade0.close_bar
+    if hasattr(trade1.lot, '__iter__'):
+        assert trade1.lot == [operator(lot) for lot in trade0.lot]
+    else:
+        assert trade1.lot == operator(trade0.lot)
 
 
 # --------------------------------------------------------------------------------
 
 
-@pytest.mark.parametrize('lot', list_lot)
+@pytest.mark.parametrize('n_orders', list_n_orders)
 @pytest.mark.parametrize('a', list_a)
-def test_mul(lot, a):
-    trade = Trade(asset='asset0', open_bar='bar0', close_bar='bar1', lot=lot)
-    trade_mul = a * trade
-
-    assert trade_mul.asset == trade.asset
-    assert trade_mul.open_bar == trade.open_bar
-    assert trade_mul.close_bar == trade.close_bar
-    assert trade_mul.lot == a * trade.lot
+def test_mul(n_orders, a):
+    trade0 = make_trade(n_orders)
+    trade1 = a * trade0
+    assert_trade_operation(trade0, trade1, lambda x: a * x)
 
 
-@pytest.mark.parametrize('lot', list_lot)
+@pytest.mark.parametrize('n_orders', list_n_orders)
 @pytest.mark.parametrize('a', list_a)
-def test_rmul(lot, a):
-    trade = Trade(asset='asset0', open_bar='bar0', close_bar='bar1', lot=lot)
-    trade_mul = trade * a
-
-    assert trade_mul.asset == trade.asset
-    assert trade_mul.open_bar == trade.open_bar
-    assert trade_mul.close_bar == trade.close_bar
-    assert trade_mul.lot == a * trade.lot
+def test_rmul(n_orders, a):
+    trade0 = make_trade(n_orders)
+    trade1 = trade0 * a
+    assert_trade_operation(trade0, trade1, lambda x: a * x)
 
 
-@pytest.mark.parametrize('lot', list_lot)
-def test_neg(lot):
-    trade = Trade(asset='asset0', open_bar='bar0', close_bar='bar1', lot=lot)
-    trade_mul = - trade
+@pytest.mark.parametrize('n_orders', list_n_orders)
+def test_neg(n_orders):
+    trade0 = make_trade(n_orders)
+    trade1 = - trade0
+    assert_trade_operation(trade0, trade1, lambda x: -x)
 
-    assert trade_mul.asset == trade.asset
-    assert trade_mul.open_bar == trade.open_bar
-    assert trade_mul.close_bar == trade.close_bar
-    assert trade_mul.lot == (-1) * trade.lot
+
+@pytest.mark.parametrize('n_orders', list_n_orders)
+@pytest.mark.parametrize('a', list_a)
+def test_truediv(n_orders, a):
+    trade0 = make_trade(n_orders)
+
+    if a != 0:
+        trade1 = trade0 / a
+        assert_trade_operation(trade0, trade1, lambda x: x / a)
+    else:
+        with pytest.raises(ZeroDivisionError):
+            trade1 = trade0 / a
+
+
+@pytest.mark.parametrize('n_orders', list_n_orders)
+@pytest.mark.parametrize('a', list_a)
+def test_floordiv(n_orders, a):
+    trade0 = make_trade(n_orders)
+
+    if a != 0:
+        trade1 = trade0 // a
+        assert_trade_operation(trade0, trade1, lambda x: x // a)
+    else:
+        with pytest.raises(ZeroDivisionError):
+            trade1 = trade0 // a
