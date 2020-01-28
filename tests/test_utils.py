54e7d831f5a241ac915d21f4f10e5458e0c9d8d7
Shota Imaki <shota.imaki@icloud.com>
2020-01-28 22:55:03 +0900

    add test bunch

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,3 +2,22 @@ import pytest
 
 
 from epymetheus.utils import Bunch
+
+
+
+def test_bunch():
+    bunch = Bunch(a='value_a')
+    bunch['b'] = 'value_b'
+    bunch.c = 'value_c'
+
+    assert bunch.a == 'value_a'
+    assert bunch.b == 'value_b'
+    assert bunch.c == 'value_c'
+    assert bunch['a'] == 'value_a'
+    assert bunch['b'] == 'value_b'
+    assert bunch['c'] == 'value_c'
+
+    assert dir(bunch) == ['a', 'b', 'c']
+
+    with pytest.raises(AttributeError):
+        d = bunch.d
