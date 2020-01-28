809dd01afe4be36d18f31c98bf65d4c81134dc4f
Shota Imaki <shota.imaki@icloud.com>
2020-01-28 09:34:09 +0900

    add and fix operator

--- epymetheus/trade.py
+++ epymetheus/trade.py
@@ -13,12 +13,12 @@ class Trade:
     ---------
     - asset : str
         Name of asset.
-    - lot : float
-        Lot to trade in unit of share.
     - open_bar
         Bar to open the trade.
     - close_bar
         Bar to close the trade.
+    - lot : float, default 1.0
+        Lot to trade in unit of share.
 
     Attributes
     ----------
@@ -31,31 +31,30 @@ class Trade:
     >>> od = datetime.date(2018, 1, 1)
     >>> cd = datetime.date(2018, 2, 1)
     >>> trade = Trade(
-    ...     asset='AAPL', lot=123.4,
-    ...     open_bar=od, close_bar=cd,
+    ...     asset='AAPL', lot=123.4, open_bar=od, close_bar=cd,
     ... )
 
     Short position:
-    >>> trade = Trade(
-    ...     asset='AAPL', lot=-45.6,
-    ...     open_bar=od, close_bar=cd,
+    >>> trade = -45.6 * Trade(
+    ...     asset='AAPL', open_bar=od, close_bar=cd,
     ... )
 
     Long-short position:
     >>> trade = Trade(
     ...     asset=['AAPL', 'MSFT'],
     ...     lot=[12.3, -45.6],
-    ...     open_bar=od, close_bar=cd,
+    ...     open_bar=od,
+    ...     close_bar=cd,
     ... )
     """
-    def __init__(self, asset, lot, open_bar, close_bar):
+    def __init__(self, asset, open_bar, close_bar, lot=1.0):
         self.asset = asset
         self.lot = lot
         self.open_bar = open_bar
         self.close_bar = close_bar
 
+        # TODO: implement them as (cached) properties
         self.n_bets = np.array(self.asset).size
-
         self._as_array = Bunch(
             asset=np.array(self.asset).reshape(-1),
             lot=np.array(self.lot).reshape(-1),
@@ -63,9 +62,23 @@ class Trade:
             close_bar=np.tile(np.array(self.close_bar), self.n_bets),
         )
 
+    @property
+    def n_orders(self):
+        if hasattr(self.lot, '__iter__'):
+            return len(self.lot)
+        else:
+            return 1
+
+    @property
+    def as_array(self):
+        return self._as_array
+
     def __mul__(self, num):
         trade = copy(self)
-        trade.lot *= num  # FIXME for multiple trades
+        if hasattr(self.lot, '__iter__'):
+            trade.lot = [lot * num for lot in trade.lot]
+        else:
+            trade.lot *= num
         return trade
 
     def __rmul__(self, num):
@@ -74,6 +87,14 @@ class Trade:
     def __neg__(self):
         return self.__mul__(-1.0)
 
-    @property
-    def as_array(self):
-        return self._as_array
+    def __truediv__(self, num):
+        print(1.0 / num)
+        return self.__mul__(1.0 / num)
+
+    def __floordiv__(self, num):
+        trade = copy(self)
+        if hasattr(self.lot, '__iter__'):
+            trade.lot = [lot // num for lot in trade.lot]
+        else:
+            trade.lot //= num
+        return trade
