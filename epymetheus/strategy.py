ecec45cf4bf68a0550011848a797814b392f58e5
Shota Imaki <shota.imaki@icloud.com>
2020-02-02 22:44:46 +0900

    remove unused, arranged order

--- epymetheus/strategy.py
+++ epymetheus/strategy.py
@@ -56,13 +56,6 @@ class TradeStrategy(metaclass=ABCMeta):
     >>> my_strategy.params
     {'my_parameter': 0.1}
 
-    Set context (optional):
-    >>> spx = ...  # Fetch S&P 500 historical prices
-    >>> my_strategy.setup(
-    ...     slippage=0.001,
-    ...     benchmark=spx,
-    ... )
-
     Run:
     >>> universe = Universe(...)
     >>> my_strategy.run(universe)
@@ -70,16 +63,6 @@ class TradeStrategy(metaclass=ABCMeta):
     def __init__(self, **kwargs):
         self.params = kwargs
         self.is_runned = False
-
-    @property
-    def name(self):
-        """Name of the strategy."""
-        return self.__class__.__name__
-
-    @property
-    def description(self):
-        """Detailed description of the strategy."""
-        return cleandoc(self.__class__.__doc__)
 
     @abstractmethod
     def logic(self, universe, **kwargs):
@@ -93,26 +76,6 @@ class TradeStrategy(metaclass=ABCMeta):
         - kwargs
             Parameters of the trade strategy.
         """
-
-    # def setup(self,
-    #           metrics=['fin_wealth'],
-    #           slippage=0.0,
-    #           benchmark=None,
-    #           ):
-    #     """
-    #     Configure the context of backtesting.
-
-    #     Parameters
-    #     ----------
-    #     - metrics
-    #     - slippage
-    #     - benchmark
-    #     """
-    #     self.context = Bunch(
-    #         metrics=metrics,
-    #         slippage=slippage,
-    #         benchmark=benchmark,
-    #     )
 
     def run(self, universe, verbose=True, save={}):
         """
@@ -131,7 +94,7 @@ class TradeStrategy(metaclass=ABCMeta):
             print('Running ... ')
 
         self.universe = universe
-        self.trades = self.generate_trades(verbose=verbose)
+        self.trades = self.__generate_trades(verbose=verbose)
 
         self.history = History(strategy=self, verbose=verbose)
         self.transaction = Transaction(strategy=self, verbose=verbose)
@@ -144,15 +107,19 @@ class TradeStrategy(metaclass=ABCMeta):
 
         self.is_runned = True
 
-        # save result TODO: check valid input
-        if save:
-            for attr, path in save.items():
-                data = pd.DataFrame(getattr(self, attr))
-                data.to_csv(path)
-
         return self
 
     # --------------------------------------------------------------------------------
+
+    @property
+    def name(self):
+        """Return name of the strategy."""
+        return self.__class__.__name__
+
+    @property
+    def description(self):
+        """Return detailed description of the strategy."""
+        return cleandoc(self.__class__.__doc__)
 
     @cached_property
     def n_trades(self):
@@ -230,7 +197,7 @@ class TradeStrategy(metaclass=ABCMeta):
     def _transaction_matrix(self):
         return pipe._transaction_matrix(self)
 
-    def generate_trades(self, verbose=True):
+    def __generate_trades(self, verbose=True):
         """
         Parameters
         ----------
@@ -243,15 +210,15 @@ class TradeStrategy(metaclass=ABCMeta):
         -------
         - list of Trade
         """
+        iter_trades = self.logic(self.universe, **self.params)
+
+        def iter_trades_verbose():
+            for i, trade in enumerate(iter_trades):
+                print(f'\rGenerating {i + 1} trades ... ', end='')
+                yield trade
+            print('Done.')
+
         if verbose:
-            def generate_trades():
-                trades = self.logic(self.universe, **self.params)
-                if not trades:
-                    return []
-                for i, trade in enumerate(trades):
-                    print(f'\rGenerating {i + 1} trades ... ', end='')
-                    yield trade
-                print('Done.')
-            return list(generate_trades())
+            return list(iter_trades_verbose() or [])
         else:
-            return list(self.logic(self.universe, **self.params))
+            return list(iter_trades or [])
