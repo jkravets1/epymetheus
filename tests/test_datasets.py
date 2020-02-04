8f1810d82b6e88f04dd453877207f39ca727a538
Shota Imaki <shota.imaki@icloud.com>
2020-02-04 17:22:47 +0900

    Update fetch_usstocks

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,12 +1,12 @@
 import pytest  # noqa
 
 import numpy as np
-from epymetheus.datasets import fetch_usstock
+from epymetheus.datasets import fetch_usstocks
 
 
 # --------------------------------------------------------------------------------
 
 
-def test_usstock():
-    universe = fetch_usstock()
+def test_usstocks():
+    universe = fetch_usstocks()
     assert not np.isnan(universe.prices.values).any(axis=None)
