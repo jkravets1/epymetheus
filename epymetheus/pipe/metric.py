f67bffb7ec9c5a358b6ad8626220c6773f6a1919
simaki <shota.imaki@icloud.com>
2020-02-05 13:54:53 +0900

    resolve importerror

--- epymetheus/pipe/metric.py
+++ epymetheus/pipe/metric.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 
 def net_exposure(strategy):
@@ -8,7 +8,7 @@ def net_exposure(strategy):
     - net_exposure : array, shape (n_bars, )
     """
     net_position = np.cumsum(strategy.transaction_matrix, axis=0)
-    return np.sum(net_position * universe.prices.values, axis=1)
+    return np.sum(net_position * strategy.universe.prices.values, axis=1)
 
 
 def abs_exposure(strategy):
@@ -18,4 +18,4 @@ def abs_exposure(strategy):
     - net_exposure : array, shape (n_bars, )
     """
     abs_position = np.abs(np.cumsum(strategy.transaction_matrix, axis=0))
-    return np.sum(abs_position * universe.prices.values, axis=1)
+    return np.sum(abs_position * strategy.universe.prices.values, axis=1)
