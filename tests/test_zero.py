022e6fde3774cccd43176249aa5341f9edc2f083
simaki <shota.imaki@icloud.com>
2020-02-05 13:31:24 +0900

    add param

--- tests/test_zero.py
+++ tests/test_zero.py
@@ -7,6 +7,7 @@ from epymetheus import TradeStrategy
 params_seed = [42]
 params_n_bars = [10, 1000]
 params_n_assets = [1, 100]
+params_verbose = [True, False]
 
 
 class VoidStrategy(TradeStrategy):
@@ -18,9 +19,10 @@ class VoidStrategy(TradeStrategy):
 @pytest.mark.parametrize('seed', params_seed)
 @pytest.mark.parametrize('n_bars', params_n_bars)
 @pytest.mark.parametrize('n_assets', params_n_assets)
-def test_void(seed, n_bars, n_assets):
+@pytest.mark.parametrize('verbose', params_verbose)
+def test_void(seed, n_bars, n_assets, verbose):
     universe = make_randomuniverse(n_bars, n_assets)
 
     with pytest.raises(RuntimeError):
-        strategy = VoidStrategy().run(universe)
+        strategy = VoidStrategy().run(universe, verbose=verbose)
         print(strategy)
