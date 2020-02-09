8024a25602a497e4803c32650886efd63fbab70d
Shota Imaki <shota.imaki@icloud.com>
2020-02-09 11:37:52 +0900

    add volatility, drop, max_drop to metric

--- epymetheus/pipe/metric.py
+++ epymetheus/pipe/metric.py
@@ -19,3 +19,53 @@ def abs_exposure(strategy):
     """
     abs_position = np.abs(np.cumsum(strategy.transaction_matrix, axis=0))
     return np.sum(abs_position * strategy.universe.prices.values, axis=1)
+
+
+def volatility(strategy):
+    """
+    Return unbiased standard deviation of gains per bars.
+
+    Returns
+    -------
+    - volatility : float
+    """
+    return np.std(np.diff(strategy.wealth.wealth), ddof=1)
+
+
+def drop(strategy):
+    """
+    Return drop of the wealth from its cumulative max.
+
+    Returns
+    -------
+    - drop : array, shape (n_bars, )
+
+    Examples
+    --------
+    >>> strategy.wealth
+    array([0, 1, 2, 1, 0])
+    >>> strategy.drop
+    array([0, 0, 0, -1, -2])
+    """
+    return strategy.wealth.wealth - np.maximum.accumulate(strategy.wealth.wealth)
+
+
+def max_drop(strategy):
+    """
+    Return maximum drop of the wealth from its cumulative max.
+
+    Returns
+    -------
+    - max_drop : float
+        Maximum drop. always non-positive.
+
+    Examples
+    --------
+    >>> strategy.wealth
+    array([0, 1, 2, 1, 0])
+    >>> strategy.drop
+    array([0, 0, 0, -1, -2])
+    >>> strategy.max_drop
+    -2
+    """
+    return min(drop(strategy))
