f85c7d07aedfbd30883cb7347f34c35de777afd2
simaki <shota.imaki@icloud.com>
2020-04-15 18:45:16 +0900

    add NotRunError

--- epymetheus/exceptions/exceptions.py
+++ epymetheus/exceptions/exceptions.py
@@ -2,3 +2,9 @@ class NoTradeError(RuntimeError):
     """
     Exception class to raise if no trades are yielded.
     """
+
+
+class NotRunError(ValueError):
+    """
+    Exception class to raise if strategy is not run.
+    """
