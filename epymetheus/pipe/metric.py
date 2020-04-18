d4494272d8c274c45961c2b83a10279129465115
simaki <shota.imaki@icloud.com>
2020-04-18 17:55:32 +0900

    init metric

--- epymetheus/pipe/metric.py
+++ epymetheus/pipe/metric.py
@@ -21,51 +21,51 @@ def abs_exposure(strategy):
     return np.sum(abs_position * strategy.universe.prices.values, axis=1)
 
 
-def volatility(strategy):
-    """
-    Return unbiased standard deviation of gains per bars.
+# def volatility(strategy):
+#     """
+#     Return unbiased standard deviation of gains per bars.
 
-    Returns
-    -------
-    - volatility : float
-    """
-    return np.std(np.diff(strategy.wealth.wealth), ddof=1)
-
-
-def drop(strategy):
-    """
-    Return drop of the wealth from its cumulative max.
-
-    Returns
-    -------
-    - drop : array, shape (n_bars, )
-
-    Examples
-    --------
-    >>> strategy.wealth
-    array([0, 1, 2, 1, 0])
-    >>> strategy.drop
-    array([0, 0, 0, -1, -2])
-    """
-    return strategy.wealth.wealth - np.maximum.accumulate(strategy.wealth.wealth)
+#     Returns
+#     -------
+#     - volatility : float
+#     """
+#     return np.std(np.diff(strategy.wealth.wealth), ddof=1)
 
 
-def max_drop(strategy):
-    """
-    Return maximum drop of the wealth from its cumulative max.
+# def drop(strategy):
+#     """
+#     Return drop of the wealth from its cumulative max.
 
-    Returns
-    -------
-    - max_drop : float
-        Maximum drop. always non-positive.
+#     Returns
+#     -------
+#     - drop : array, shape (n_bars, )
 
-    Examples
-    --------
-    >>> strategy.wealth
-    array([0, 1, 2, 1, 0])
-    >>> strategy.drop
-    array([0, 0, 0, -1, -2])
-    >>> strategy.max_drop
-    -2
-    """
-    return min(drop(strategy))
+#     Examples
+#     --------
+#     >>> strategy.wealth
+#     array([0, 1, 2, 1, 0])
+#     >>> strategy.drop
+#     array([0, 0, 0, -1, -2])
+#     """
+#     return strategy.wealth.wealth - np.maximum.accumulate(strategy.wealth.wealth)
+
+
+# def max_drop(strategy):
+#     """
+#     Return maximum drop of the wealth from its cumulative max.
+
+#     Returns
+#     -------
+#     - max_drop : float
+#         Maximum drop. always non-positive.
+
+#     Examples
+#     --------
+#     >>> strategy.wealth
+#     array([0, 1, 2, 1, 0])
+#     >>> strategy.drop
+#     array([0, 0, 0, -1, -2])
+#     >>> strategy.max_drop
+#     -2
+#     """
+#     return min(drop(strategy))
