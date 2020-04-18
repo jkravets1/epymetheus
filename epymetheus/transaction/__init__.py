95e704d83b8156065f8c0a565dba83f8af57ca25
simaki <shota.imaki@icloud.com>
2020-04-18 17:40:05 +0900

    comment out transaction

--- epymetheus/transaction/__init__.py
+++ epymetheus/transaction/__init__.py
@@ -1,3 +1,3 @@
 # flake8: noqa
 
-from .transaction import Transaction
+# from .transaction import Transaction
--- epymetheus/transaction/transaction.py
+++ epymetheus/transaction/transaction.py
@@ -1,44 +1,25 @@
-from time import time
-
-from epymetheus.utils import TradeResult
+# from epymetheus.utils import TradeResult
 
 
-class Transaction(TradeResult):
-    """
-    Represent transaction history.
+# class Transaction(TradeResult):
+#     """
+#     Represent transaction history.
 
-    Attributes
-    ----------
-    - (name of assets) : array, (n_bars, )
-        Transaction of each asset.
-    """
-    @classmethod
-    def from_strategy(cls, strategy, verbose=True):
-        """
-        Initialize self from strategy.
+#     Attributes
+#     ----------
+#     - (name of assets) : array, (n_bars, )
+#         Transaction of each asset.
+#     """
+#     @classmethod
+#     def from_strategy(cls, strategy):
+#         """
+#         Initialize self from strategy.
 
-        Parameters
-        ----------
-        - strategy : TradeStrategy
-        - verbose : bool
+#         Parameters
+#         ----------
+#         - strategy : TradeStrategy
 
-        Returns
-        -------
-        transaction : Transaction
-        """
-        if verbose:
-            msg = 'Evaluating transaction'
-            print(f'{msg:<22} ... ', end='')
-            begin_time = time()
-
-        transaction = cls()
-        transaction.bars = strategy.universe.bars
-        transaction_matrix = strategy.transaction_matrix
-
-        for asset_id, asset in enumerate(strategy.universe.assets):
-            setattr(transaction, asset, transaction_matrix[:, asset_id])
-
-        if verbose:
-            print(f'Done. (Runtime : {time() - begin_time:.2f} sec)')
-
-        return transaction
+#         Returns
+#         -------
+#         transaction : Transaction
+#         """
