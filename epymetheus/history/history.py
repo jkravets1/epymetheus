d49b7ae26151309a86894157b9199723fa90ce2b
simaki <shota.imaki@icloud.com>
2020-04-18 09:30:29 +0900

    add doc

--- epymetheus/history/history.py
+++ epymetheus/history/history.py
@@ -72,19 +72,14 @@ class History(TradeResult):
         return history
 
     @staticmethod
-    def _get_trade_index(strategy):
+    def _get_order_index(strategy):
         """
         Return order_index of each order.
 
-        Parameters
-        ----------
-        - strategy : Tradestrategy
-            with the following attributes:
-            * trades
-
         Returns
         -------
-        trade_index : array, shape (n_orders, )
+        array_order_index : array, shape (n_orders, )
+            array([0, 1, ..., n_orders - 1])
 
         Examples
         --------
@@ -92,8 +87,28 @@ class History(TradeResult):
         ...     Trade(asset=['Asset0', 'Asset1'], ...),
         ...     Trade(asset=['Asset2'], ...),
         ... ]
-        >>> strategy.trade_index
-        array([ 0, 0, 1])
+        >>> History()._get_order_index(strategy)
+        array([  0  1  2])
+        """
+        return np.arange(strategy.n_orders)
+
+    @staticmethod
+    def _get_trade_index(strategy):
+        """
+        Return trade_index of each order.
+
+        Returns
+        -------
+        array_trade_index : array, shape (n_orders, )
+
+        Examples
+        --------
+        >>> strategy.trades = [
+        ...     Trade(asset=['Asset0', 'Asset1'], ...),
+        ...     Trade(asset=['Asset2'], ...),
+        ... ]
+        >>> History()._get_trade_index(strategy)
+        array([  0  0  1])
         """
         return np.repeat(
             np.arange(strategy.n_trades),
@@ -101,13 +116,13 @@ class History(TradeResult):
         )
 
     @staticmethod
-    def _get_order_index(strategy):
+    def _get_asset(strategy):
         """
-        Return order_index of each order.
+        Return asset of each order.
 
         Returns
         -------
-        array([0, 1, ..., n_orders - 1]) : shape (n_orders, )
+        array_asset : array, shape (n_orders, )
 
         Examples
         --------
@@ -115,13 +130,9 @@ class History(TradeResult):
         ...     Trade(asset=['Asset0', 'Asset1'], ...),
         ...     Trade(asset=['Asset2'], ...),
         ... ]
-        >>> strategy.trade_index
-        array([ 0, 1, 2])
+        >>> History()._get_asset(strategy)
+        array([  'Asset0'  'Asset1'  'Asset2'])
         """
-        return np.arange(strategy.n_orders)
-
-    @staticmethod
-    def _get_asset(strategy):
         return np.concatenate([
             trade.array_asset for trade in strategy.trades
         ])
@@ -133,7 +144,7 @@ class History(TradeResult):
 
         Returns
         -------
-        lot : array, shape (n_orders, )
+        array_lot : array, shape (n_orders, )
 
         Examples
         --------
@@ -141,8 +152,8 @@ class History(TradeResult):
         ...     Trade(lot=[1, -2], ...),
         ...     Trade(lot=[3], ...),
         ... ]
-        >>> strategy.lots
-        array([  1, -2,  3])
+        >>> History()._get_lot(strategy)
+        array([  1 -2  3])
         """
         return np.concatenate([
             trade.array_lot for trade in strategy.trades
@@ -150,6 +161,22 @@ class History(TradeResult):
 
     @staticmethod
     def _get_open_bar(strategy):
+        """
+        Return open_bar of each order.
+
+        Returns
+        -------
+        array_open_bar : array, shape (n_orders, )
+
+        Examples
+        --------
+        >>> strategy.trades = [
+        ...     Trade(open_bar='Bar0', asset=['Asset0', 'Asset1'], ...),
+        ...     Trade(open_bar='Bar1', asset=['Asset2'], ...),
+        ... ]
+        >>> History()._get_open_bar(strategy)
+        array([  'Bar0'  'Bar0'  'Bar1'])
+        """
         return np.repeat(
             [trade.open_bar for trade in strategy.trades],
             [trade.n_orders for trade in strategy.trades],
@@ -157,6 +184,22 @@ class History(TradeResult):
 
     @staticmethod
     def _get_close_bar(strategy):
+        """
+        Return close_bar of each order.
+
+        Returns
+        -------
+        array_close_bar : array, shape (n_orders, )
+
+        Examples
+        --------
+        >>> strategy.trades = [
+        ...     Trade(close_bar='Bar0', asset=['Asset0', 'Asset1'], ...),
+        ...     Trade(close_bar='Bar1', asset=['Asset2'], ...),
+        ... ]
+        >>> History()._get_close_bar(strategy)
+        array([  'Bar0'  'Bar0'  'Bar1'])
+        """
         return np.repeat(
             [trade.close_bar for trade in strategy.trades],
             [trade.n_orders for trade in strategy.trades],
@@ -164,6 +207,22 @@ class History(TradeResult):
 
     @staticmethod
     def _get_shut_bar(strategy):
+        """
+        Return shut_bar of each order.
+
+        Returns
+        -------
+        array_shut_bar : array, shape (n_orders, )
+
+        Examples
+        --------
+        >>> strategy.trades = [
+        ...     Trade(shut_bar='Bar0', asset=['Asset0', 'Asset1'], ...),
+        ...     Trade(shut_bar='Bar1', asset=['Asset2'], ...),
+        ... ]
+        >>> History()._get_shut_bar(strategy)
+        array([  'Bar0'  'Bar0'  'Bar1'])
+        """
         return np.repeat(
             [trade.shut_bar for trade in strategy.trades],
             [trade.n_orders for trade in strategy.trades],
@@ -171,6 +230,23 @@ class History(TradeResult):
 
     @staticmethod
     def _get_take(strategy):
+        """
+        Return take of each order.
+
+        Returns
+        -------
+        array_take : numpy.array, shape (n_orders, )
+
+        Examples
+        --------
+        >>> strategy.trades
+        ... [
+        ...     Trade(take=1, asset=['Asset0', 'Asset1'], ...),
+        ...     Trade(take=2, asset=['Asset2'], ...),
+        ... ]
+        >>> History()._get_take
+        array([  1  1  2])
+        """
         return np.repeat(
             [trade.take for trade in strategy.trades],
             np.arange(strategy.n_trades)
@@ -178,6 +254,22 @@ class History(TradeResult):
 
     @staticmethod
     def _get_stop(strategy):
+        """
+        Return stop of each order.
+
+        Returns
+        -------
+        array_stop : numpy.array, shape (n_orders, )
+
+        Examples
+        --------
+        >>> strategy.trades = [
+        ...     Trade(stop=-1, asset=['Asset0', 'Asset1'], ...),
+        ...     Trade(stop=-2, asset=['Asset2'], ...),
+        ... ]
+        >>> strategy.stop
+        array([ -1 -1 -2])
+        """
         return np.repeat(
             [trade.stop for trade in strategy.trades],
             np.arange(strategy.n_trades),
