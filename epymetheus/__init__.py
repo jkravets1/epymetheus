47bc7d6886e9dc366bb03249e27c3aa00fd5c19c
simaki <shota.imaki@icloud.com>
2020-04-18 11:02:17 +0900

    resolve importerror

--- epymetheus/__init__.py
+++ epymetheus/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
 
-from epymetheus.strategy import TradeStrategy
-from .trade import Trade
 from epymetheus.history import History
+from epymetheus.strategy import TradeStrategy
+from epymetheus.trade import Trade
 from epymetheus.universe import Universe
+from epymetheus.wealth import Wealth
