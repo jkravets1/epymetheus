f302493a2f8f42930435673e5af19408c2cd51bf
simaki <shota.imaki@icloud.com>
2020-04-18 14:22:44 +0900

    add pandas test

--- tests/history/test_pandas.py
+++ tests/history/test_pandas.py
@@ -1,12 +1,15 @@
 import pytest
 
 import numpy as np
+from numpy import array_equal
+import pandas as pd
+from pandas.testing import assert_index_equal
 
 from epymetheus import History
 from epymetheus.datasets import make_randomwalk
 from epymetheus.benchmarks import RandomTrader
 
-columns = [
+columns = pd.Index([
     'trade_id',
     'asset',
     'lot',
@@ -16,10 +19,22 @@ columns = [
     'take',
     'stop',
     'pnl',
-]
+])
 
 
 # --------------------------------------------------------------------------------
+
+
+def test_type():
+    """
+    Test if history.to_dataframe is `pandas.DataFrame`.
+    """
+    universe = make_randomwalk()
+    strategy = RandomTrader().run(universe)
+
+    df_history = History(strategy).to_dataframe()
+
+    assert isinstance(df_history, pd.DataFrame)
 
 
 def test_columns():
@@ -32,7 +47,21 @@ def test_columns():
     df_history = History(strategy).to_dataframe()
 
     assert df_history.index.name == 'order_id'
-    assert list(df_history.columns) == columns
+    assert_index_equal(df_history.columns, columns)
+
+
+def test_index():
+    """
+    Test if value of history.to_dataframe is correct
+    """
+    universe = make_randomwalk()
+    strategy = RandomTrader().run(universe)
+
+    history = History(strategy)
+    df_history = history.to_dataframe()
+
+    index_expected = pd.Index(range(df_history.index.size), name='order_id')
+    assert_index_equal(df_history.index, index_expected)
 
 
 def test_value():
@@ -46,4 +75,4 @@ def test_value():
     df_history = history.to_dataframe()
 
     for c in columns:
-        assert np.array_equal(df_history[c], history[c])
+        assert array_equal(df_history[c], history[c])
--- tests/wealth/test_pandas.py
+++ tests/wealth/test_pandas.py
@@ -1,25 +1,25 @@
 import pytest  # noqa: F401
 
 import numpy as np
+from numpy import array_equal
+import pandas as pd
+from pandas.testing import assert_index_equal, assert_series_equal
 
 from epymetheus import Wealth
 from epymetheus.datasets import make_randomwalk
 from epymetheus.benchmarks import RandomTrader
 
-columns = [
-    'trade_id',
-    'asset',
-    'lot',
-    'open_bar',
-    'close_bar',
-    'shut_bar',
-    'take',
-    'stop',
-    'pnl',
-]
-
 
 # --------------------------------------------------------------------------------
+
+
+def test_series_type():
+    universe = make_randomwalk()
+    strategy = RandomTrader().run(universe)
+    wealth = Wealth(strategy)
+    series_wealth = wealth.to_series()
+
+    assert isinstance(series_wealth, pd.Series)
 
 
 def test_series_value():
@@ -29,6 +29,18 @@ def test_series_value():
     series_wealth = wealth.to_series()
 
     assert np.array_equal(series_wealth, wealth['wealth'])
+
+
+def test_dataframe_type():
+    """
+    Test if history.to_dataframe is `pandas.DataFrame`.
+    """
+    universe = make_randomwalk()
+    strategy = RandomTrader().run(universe)
+    wealth = Wealth(strategy)
+    df_wealth = wealth.to_dataframe()
+
+    assert isinstance(df_wealth, pd.DataFrame)
 
 
 def test_dataframe_columns():
@@ -41,8 +53,19 @@ def test_dataframe_columns():
     df_wealth = wealth.to_dataframe()
 
     assert df_wealth.index.name == 'bars'
-    assert list(df_wealth.columns) == ['wealth']
+    assert_index_equal(df_wealth.columns, pd.Index(['wealth']))
 
+
+def test_dataframe_index():
+    """
+    Test if value of history.to_dataframe is correct
+    """
+    universe = make_randomwalk()
+    strategy = RandomTrader().run(universe)
+    wealth = Wealth(strategy)
+    df_wealth = wealth.to_dataframe()
+
+    assert_index_equal(df_wealth.index, universe.bars, check_names=False)
 
 def test_dataframe_value():
     """
@@ -53,4 +76,4 @@ def test_dataframe_value():
     wealth = Wealth(strategy)
     df_wealth = wealth.to_dataframe()
 
-    assert np.array_equal(df_wealth['wealth'], wealth.wealth)
+    assert array_equal(df_wealth['wealth'].values, wealth.wealth)
