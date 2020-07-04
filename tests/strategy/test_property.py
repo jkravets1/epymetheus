09dccd8e2e003d33e46766fa145a4d1f593daec9
Shota Imaki <shota.imaki@icloud.com>
2020-07-04 14:11:29 +0900

    refactor

--- tests/strategy/test_property.py
+++ tests/strategy/test_property.py
@@ -21,38 +21,80 @@ class SampleStrategy(Strategy):
         yield Trade(asset=["A2", "A3"])
 
 
-# --------------------------------------------------------------------------------
-
-
-class TestProperty:
-
-    universe = Universe(pd.DataFrame({f"A{i}": range(10) for i in range(4)}))
-
-    def test_name(self):
+class TestName:
+    """
+    Test `Strategy.name`.
+    """
+    def test_value(self):
         strategy = SampleStrategy()
         assert strategy.name == "SampleStrategy"
 
-    def test_description(self):
+
+class TestDescription:
+    """
+    Test `Strategy.description`.
+    """
+    def test_value(self):
         strategy = SampleStrategy()
         assert (
             strategy.description
             == "This is my favorite strategy.\n\nEverybody loves this strategy."
         )
 
-    def test_params(self):
-        strategy = SampleStrategy(param0=0.0, param1=1.0)
-        assert strategy.params == {"param0": 0.0, "param1": 1.0}
 
-    def test_is_run(self):
+class TestIsRun:
+    """
+    Test `Strategy.is_run`.
+    """
+    universe = Universe(pd.DataFrame({f"A{i}": range(10) for i in range(4)}))
+
+    def test_value(self):
         strategy = SampleStrategy()
         assert strategy.is_run == False
         strategy.run(self.universe)
         assert strategy.is_run == True
 
-    def test_n_trades(self):
+
+class TestParams:
+    """
+    Test `Strategy.params`.
+    """
+    def test_value(self):
+        strategy = SampleStrategy(param0=0.0, param1=1.0)
+        assert strategy.params == {"param0": 0.0, "param1": 1.0}
+
+
+class TestNTrades:
+    """
+    Test `Strategy.n_trades`.
+    """
+    universe = Universe(pd.DataFrame({f"A{i}": range(10) for i in range(4)}))
+
+    def test_value(self):
         strategy = SampleStrategy().run(self.universe)
         assert strategy.n_trades == 2
 
-    def test_n_orders(self):
+
+class TestNOrders:
+    """
+    Test `Strategy.n_orders`.
+    """
+    universe = Universe(pd.DataFrame({f"A{i}": range(10) for i in range(4)}))
+
+    def test_value(self):
         strategy = SampleStrategy().run(self.universe)
         assert strategy.n_orders == 4
+
+
+class TestHistory:
+
+    def test_value(self):
+        # TODO: move from tests/history
+        pass
+
+
+class TestWealth:
+
+    def test_value(self):
+        # TODO: move from tests/history
+        pass
