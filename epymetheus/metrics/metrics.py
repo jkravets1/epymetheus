a5f7e5d27186fb0a92a25f345bb96c6820b4bb09
Shota Imaki <shota.imaki@icloud.com>
2020-07-05 12:52:29 +0900

    black

--- epymetheus/metrics/metrics.py
+++ epymetheus/metrics/metrics.py
@@ -107,6 +107,7 @@ class AverageReturn(Metric):
     - rate : bool
     - n : int
     """
+
     def __init__(self, rate=False, n=1, **kwargs):
         super().__init__(**kwargs)
         self.rate = rate
@@ -131,7 +132,6 @@ class AverageReturn(Metric):
     def result(self, strategy):
         series_wealth = strategy.wealth.wealth + strategy.budget
         return self._result_from_wealth(series_wealth)
-
 
 
 class FinalWealth(Metric):
@@ -198,7 +198,7 @@ class Drawdown(Metric):
         result = series_wealth - cummax
 
         if self.rate:
-            result /= (cummax + EPSILON)
+            result /= cummax + EPSILON
 
         return result
 
@@ -304,7 +304,7 @@ class SharpeRatio(Metric):
         average_return = AverageReturn(rate=self.rate, n=self.n).result(strategy)
         volatility = Volatility(rate=self.rate, n=self.n).result(strategy)
         volatility = max(volatility, EPSILON)
-        result =  (average_return - self.risk_free_return) / volatility
+        result = (average_return - self.risk_free_return) / volatility
         return result
 
 
