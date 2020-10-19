6dd176b477e25803a6ce9d72687e93c4d5b134e5
Shota Imaki <shota.imaki@icloud.com>
2020-10-19 11:10:07 +0900

    update test.sh

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ python3 -m pytest --doctest-modules epymetheus
 python3 -m pytest --doctest-modules tests
 
 python3 -m flake8 epymetheus
-python3 -m black --check epymetheus
-python3 -m isort --check --force-single-line-imports epymetheus
+python3 -m black --check epymetheus || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m black epymetheus
+python3 -m isort --check --force-single-line-imports epymetheus || read -p "Run formatter? (y/N): " yn; [[ $yn = [yY] ]] && python3 -m isort --force-single-line-imports epymetheus
